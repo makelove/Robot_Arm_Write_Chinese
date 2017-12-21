4b0d20a14849888b8a55d80138c58515b5fc00ef
play4fun <play4fun@foxmail.com>
2017-12-21 23:07:04 +0800

    uArm_draw1

---
+++ py/uArm_draw1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2017/12/21 20:00
+# @Author  : play4fun
+# @File    : uArm_draw1.py
+# @Software: PyCharm
+
+"""
+uArm_draw1.py:
+"""
+
+import pickle
+from time import sleep
+
+# 读
+with open('Chinese_strokes', 'rb') as f:
+    data = pickle.load(f)
+
+#获取笔尖高度
+
+#输入文字，想draw哪个字？
+
+#修改坐标系
+
+#uArm机械臂运动
