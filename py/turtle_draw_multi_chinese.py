90892432a0e76ed978a0a88362fcf361b56fb348
play4fun <play4fun@foxmail.com>
2018-01-08 15:52:33 +0800

    中心点为任意点

--- py/turtle_draw_multi_chinese.py
+++ py/turtle_draw_multi_chinese.py
@@ -18,18 +18,20 @@ from utils import draw_rect, draw_line
 with open('Chinese_strokes', 'rb') as f:
     data = pickle.load(f)
 
+center_point = (20, 210)  # 中心点为任意点
+
 turtle.screensize(100, 100, "white")
 # turtle.setpos(-100, 0)
 # turtle.down()
 # turtle.goto(100, 0)
 # turtle.up()
-draw_line(-100, 0, 100, 0)
+draw_line(-100 + center_point[0], 0 + center_point[1], 100 + center_point[0], 0 + center_point[1])
 #
 # turtle.setpos(0, -100)
 # turtle.down()
 # turtle.goto(0, 100)
 # turtle.up()
-draw_line(0, -100, 0, 100)
+draw_line(0 + center_point[0], -100 + center_point[1], 0 + center_point[0], 100 + center_point[1])
 
 sleep(2)
 #
@@ -39,26 +41,26 @@ sentense = '新年快乐'
 num = len(sentense)
 width = 100  # 一个字的宽度 100*100
 # all_width = width * num  # 总长度
-# 中心点为原点
+
 
 
 # counter = 0
 for i, word in enumerate(sentense):
     # turtle.clear()
-    x_pos = width * (i - num / 2)
-    draw_rect(x=x_pos, y=0, width=width)
+    x_pos = width * (i - num / 2) + center_point[0]
+    draw_rect(x=x_pos, y=0 + center_point[1], width=width)
 
     print('----------', word)
     strokes = data[word]
     for st in strokes:
         print('--')
 
-        turtle.goto(x_pos + st[0]['x'] / 10, -st[0]['y'] / 10)
+        turtle.goto(x_pos + st[0]['x'] / 10, -st[0]['y'] / 10 + center_point[1])
         turtle.down()
 
         for po in st:
-            print(x_pos + po['x'], po['y'])
-            turtle.goto(x_pos + po['x'] / 10, -po['y'] / 10)
+            print(x_pos + po['x'], po['y'] + center_point[1])
+            turtle.goto(x_pos + po['x'] / 10, -po['y'] / 10 + center_point[1])
         turtle.up()
     # counter += 1
     sleep(0.5)
